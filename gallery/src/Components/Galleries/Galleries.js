a185c9018bd383f08fa63b4ab824ce1bdb88a464
romie151 <romiezelaya@ac.c4q.nyc>
2018-03-05 00:23:22 -0500

    home page style changes

--- gallery/src/Components/Galleries/Galleries.js
+++ gallery/src/Components/Galleries/Galleries.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import {Link, Route, Switch} from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 
-class Galleries extends Component{
-    constructor(props){
+class Galleries extends Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        const {resultArr} = this.props;
-        return(
+    render() {
+        const { resultArr } = this.props;
+        return (
             <div >
                 <div id="galleryList">
-                {resultArr.map ((el, i) => (
-                    <div> <p id="renderedGallery" key={i}> {el.name} - {el.address1} 
-                    </p></div>
-                ))}
+                    {resultArr.map((el, i) => (
+                        <div id="render">
+                            <p id="renderedGallery" key={i}> {el.name} - {el.address1}</p>
+                        </div>
+                    ))}
 
                 </div>
             </div>
