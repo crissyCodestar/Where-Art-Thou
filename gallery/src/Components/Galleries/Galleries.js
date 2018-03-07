3fdd2ed7d04a813b04662832a02c1220464c31c6
romie151 <romiezelaya@ac.c4q.nyc>
2018-03-06 21:24:21 -0500

    fix again

--- gallery/src/Components/Galleries/Galleries.js
+++ gallery/src/Components/Galleries/Galleries.js
@@ -11,25 +11,42 @@ class Galleries extends Component {
         }
     }
 
-    render() {
-        const { resultArr } = this.props;
-        return (
-            <div >
-                <div id="galleryList">
-                <p id="gallery"><strong>Gallery List</strong></p>
-                    {resultArr.map((el, i) => (
-                        <div id="render">
-                            <p id="renderedGallery" key={i}> {el.name} - {el.address1}</p>
-                        </div>
-                    ))}
 
-                </div>
+
+    onArtClick = art => {
+      console.log("clicked on: ", art);
+      this.props.onArtClick(art);
+      this.setState({ selectedArtId: art.unique_key });
+    };
+
+    render(){
+      console.log("In GALL: ",this.state.selectedArtId)
+        const {resultArr} = this.props;
+        return(
+            <div>
+                <ol id="galleryList">
+                {resultArr.map ((art) => (
+                  <div id="divLi">
+                    <li> <span className="link-title"
+                      onClick={() => this.onArtClick(art)}>
+                    {art.name}
+                    </span><br />
+                    <span className="link-excerpt">
+                     {art.address1}
+                    </span>
+                    </li>
+                    </div>
+                ))}
+
+
+                </ol>
             </div>
         )
     }
 }
 
 export default Galleries;
+
 
 // art={art}
 // key={art.unique_key}
